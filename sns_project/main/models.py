86527b634e31e5ace2e59eda54f225d4863c9ff4
alswn9938@gmail.com <alswn9938@gmail.com>
2021-06-18 01:17:54 +0900

    과제제출

--- sns_project/main/models.py
+++ sns_project/main/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Post(models.Model):
     title=models.CharField(max_length=200)
-    writer=models.CharField(max_length=100)
+    writer=models.ForeignKey(User,on_delete=models.CASCADE)
     pub_date=models.DateTimeField()
     body=models.TextField()
     image=models.ImageField(upload_to='post/',blank=True, null=True)
+    
 
     def __str__(self):
         return self.title #입력한 데이터가 호출되면 제목이 대표값으로 나오게 한다.
--- sns_project/sns_project/urls.py
+++ sns_project/sns_project/urls.py
@@ -20,9 +20,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-app_name='main'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-
+    path('accounts/',include('allauth.urls')),
+    path('users/',include('users.urls')),
     ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
