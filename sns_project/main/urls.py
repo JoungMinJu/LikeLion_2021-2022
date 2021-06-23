697ec6fc1dfb9adf637f72cfe7cad1c08e38496b
minju <alswn9938@gmail.com>
2021-06-23 21:21:13 +0900

    과제

--- sns_project/main/urls.py
+++ sns_project/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 
+
 app_name='main'
 urlpatterns = [
     path('',showmain,name='main'),
@@ -12,5 +13,8 @@ urlpatterns = [
     path('edit/<str:id>',edit,name='edit'),
     path('update/<str:id>',update,name='update'),
     path('delete/<str:id>', delete, name='delete'),
-
+    path('<str:post_id>/create_comment',create_comment, name="create_comment"),
+    path('<str:post_id>/<str:comment_id>/edit_comment', edit_comment, name='edit_comment'),
+    path('<str:comment_id>/delete_comment', delete_comment, name="delete_comment"),
+    path('<str:post_id>/<str:comment_id>/update_comment', update_comment, name='update_comment'),
     ]
--- sns_project/main/views.py
+++ sns_project/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Post
+from .models import Post,Comment
 from django.utils import timezone
 
 # Create your views here.
@@ -15,7 +15,8 @@ def showfirst(request):
 
 def detail(request,id):
     post=get_object_or_404(Post,pk=id)
-    return render(request,'main/detail.html',{'post':post})
+    all_comments=post.comments.all().order_by('-created_at')
+    return render(request,'main/detail.html',{'post':post, 'comments':all_comments})
 
 def new(request):
     return render(request,'main/new.html')
@@ -48,3 +49,34 @@ def delete(request, id):
     delete_post=Post.objects.get(id=id)
     delete_post.delete()
     return redirect('main:main')
+
+def create_comment(request, post_id):
+	if request.method == "POST":
+		post = get_object_or_404(Post, pk=post_id)
+		current_user = request.user
+		comment_content = request.POST.get('content')
+		Comment.objects.create(content=comment_content, writer=current_user, post=post)
+	return redirect('main:detail', post_id)
+
+
+def edit_comment(request, post_id, comment_id):
+    edit_post=Post.objects.get(id=post_id)
+    edit_comment=Comment.objects.get(id=comment_id)
+    return render(request, 'main/edit_comment.html',{'post': edit_post, 'comment':edit_comment})
+    ##edit_comment.html에 수정하고자하는 포스트랑 수정하고자하는 comment값을 보냈슴
+
+
+def delete_comment(request, comment_id):
+    delete_comment=Comment.objects.get(id=comment_id)
+    delete_comment.delete()
+    return redirect('main:main')
+
+
+def update_comment(request, post_id,comment_id):
+    if request.method=='POST':
+        post=get_object_or_404(Post,pk=post_id)
+        comment=Comment.objects.filter(pk=comment_id)
+        current_user=request.user
+        comment_content=request.POST.get('content')
+        comment.update(content=comment_content,writer=current_user, post=post)
+    return redirect('main:detail', post_id)
